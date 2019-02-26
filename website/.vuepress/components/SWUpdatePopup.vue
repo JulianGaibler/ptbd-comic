85c3a619c2ae949615191de227f0ba3894c5293c
Juliana Gaibler <git@juliana.me>
2019-02-26 14:55:22 +0100

    :sparkles: PWA-Plugin plus Popup

---
+++ website/.vuepress/components/SWUpdatePopup.vue
@@ -0,0 +1,16 @@
+<template>
+  <SWUpdatePopup>
+    <div slot-scope="{ reload }" class="page-update">
+      <p>This website has been updated. Reload to see what's new!</p>
+      <button class="ptbd-button" @click="reload">Reload Page</button>
+    </div>
+  </SWUpdatePopup>
+</template>
+
+<script>
+import SWUpdatePopup from '@vuepress/plugin-pwa/lib/SWUpdatePopup.vue'
+
+export default {
+  components: { SWUpdatePopup }
+}
+</script>
