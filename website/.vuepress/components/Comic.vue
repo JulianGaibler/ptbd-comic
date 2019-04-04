40110f1914f4247a4c295265f2c6264688418cc5
JulianWels <julian@jwels.berlin>
2019-04-04 16:34:56 +0200

    :bug: Fixed #2

--- website/.vuepress/components/Comic.vue
+++ website/.vuepress/components/Comic.vue
@@ -1,17 +1,18 @@
 <template>
-	<article class="comicpage">
-		<h1>{{info.title}}</h1>
-		<time :datetime="info.date">{{getDateStr(info.date)}}</time>
-		<div :class="classes">
-			<ProgressivePanel v-for="panel in panels" :img="panel" />
-		</div>
-		<ComicNav 
-			:prev="$page.frontmatter.prevComic"
-			:next="$page.frontmatter.nextComic"
-			:share="share"
-		/>
+    <article class="comicpage">
+        <h1>{{info.title}}</h1>
+        <time :datetime="info.date">{{getDateStr(info.date)}}</time>
+        <div :class="classes">
+            <ProgressivePanel v-for="panel in panels" :img="panel" />
+        </div>
+        <ComicNav 
+            :prev="$page.frontmatter.prevComic"
+            :next="$page.frontmatter.nextComic"
+            :share="share"
+            :comicID="info.comicID"
+        />
         <div class="description">
-        	<slot />
+            <slot />
         </div>
     </article>
 </template>
@@ -22,20 +23,20 @@ import ComicNav from './Comic/ComicNav.vue'
 import { DateTime } from "luxon";
 
 export default {
-	props: ['info', 'panels', 'share'],
-	components: { ProgressivePanel, ComicNav },
-    data: function () {
-    	const cid = this.info.comicID;
-    	const panelsLen = this.panels.length;
-    	let info = [];
-    	if (cid < 46 && panelsLen < 3) info = ['legacy']
-    	else if (panelsLen < 3) info = ['col1']
+    props: ['info', 'panels', 'share'],
+    components: { ProgressivePanel, ComicNav },
+    data() {
+        const cid = this.info.comicID;
+        const panelsLen = this.panels.length;
+        let info = [];
+        if (cid < 46 && panelsLen < 3) info = ['legacy']
+        else if (panelsLen < 3) info = ['col1']
         return {
            classes: ['panels', ...info]
         }
     },
     methods: {
-    	getDateStr(date) {
+        getDateStr(date) {
             return DateTime.fromISO(date).toLocal().toFormat('dd LLL yyyy')
         },
     },
